7292d9f31ecd990e7a3744bcd5ed0b595ad7c802
Steave-Choi <csh005@hanmail.net>
2022-03-05 14:06:38 +0900

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
